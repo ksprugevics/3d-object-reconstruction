ec06d789111c55b47e7d397aff6dc7fd992a63f7
ksprugevics <klavs.dev@gmail.com>
2025-03-22 12:52:42 +0200

    Defined MotorControllers with actual pins

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,16 @@
 
 DistanceSensor* mySensor;
 MotorController platform(6, 7, 5);
+MotorController actuator(8, 9, 10);
 
 void setup() {
     Serial.begin(9600);
-    // mySensor = new DistanceSensor(VL53L1X::Long, 50000, 50, 500, 400000, false);
+    mySensor = new DistanceSensor(VL53L1X::Long, 50000, 50, 500, 400000, false);
+    platform.spinClockwise();
+    actuator.spinClockwise();
 }
 
 void loop() {
-    // Serial.println(mySensor->measure());
-    platform.spinClockwise();
+    Serial.println(mySensor->measure());
+    delay(100);
 }
