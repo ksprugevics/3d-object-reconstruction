ebf881ecf392d4d7be748d794b72c59e926f6b7d
ksprugevics <klavs.dev@gmail.com>
2025-04-01 20:31:55 +0300

    Experiments with step motor

---
+++ reconstruction/reconstruct_steps.py
@@ -0,0 +1,40 @@
+# Converts a list of distance measurements (from a static distance sensor), of a single rotation of an object, to create a 2D scatter plot.
+# Inputs:
+# SCAN_FILENAME - file that contains a list of distance measurements in mm
+# SCATTER_INTERVAL_MILLIS - interval between each measurement in milliseconds
+# FULL_ROTATION_INTERVAL_MILLIS - interval of the full rotation in milliseconds
+
+import plotly.express as px
+import math
+
+def readFileIntoList(filename):
+    with open(filename) as scan:
+        return [int(line.rstrip()) for line in scan]
+
+def calculateDegreeStep(point_count):
+    return 360 / 400;
+
+def polarCoordinatesToCartesian(length, degree):
+    return [math.cos(math.radians(degree)) * length, math.sin(math.radians(degree)) * length]
+
+def convertScanDataToCartesianData(scan_data, degree_step):
+    coords_x = []
+    coords_y = []
+    degree = 0
+
+    for entry in scan_data:
+        cartesian_coords = polarCoordinatesToCartesian(entry, degree)
+        coords_x.append(cartesian_coords[0])
+        coords_y.append(cartesian_coords[1])
+        degree += degree_step
+
+    return [coords_x, coords_y]
+
+SCAN_FILENAME = "reconstruction//scans//tinderbox_5.txt"
+
+scan_data = readFileIntoList(SCAN_FILENAME)
+degree_step = calculateDegreeStep(len(scan_data))
+processed_scan = convertScanDataToCartesianData(scan_data, degree_step)
+
+fig = px.scatter(x=processed_scan[1], y=processed_scan[0])
+fig.show()
--- src/main.cpp
+++ src/main.cpp
@@ -4,43 +4,24 @@
 
 const int STEPS_IN_FULL_REVOLUTION = 200;
 
-// DistanceSensor* mySensor;
-// MotorController actuator(8, 9, 10);
-
-StepMotorController stepper(STEPS_IN_FULL_REVOLUTION, StepMotorController::SteppingMode::FULL, false, 200, 8, 12, 13, 9, 10, 11);
+DistanceSensor* mySensor;
+StepMotorController stepper(STEPS_IN_FULL_REVOLUTION, StepMotorController::SteppingMode::HALF, false, 200, 8, 12, 13, 9, 10, 11);
 
 void setup() {
     Serial.begin(9600);
-    // mySensor = new DistanceSensor(VL53L1X::Long, 50000, 50, 500, 400000, false);
-    delay(1000);
+    mySensor = new DistanceSensor(VL53L1X::Long, 70000, 70, 500, 400000, false);
+    Serial.println(mySensor->measure());
     Serial.println("Starting measurement");
-    // Serial.println(mySensor->measure());
+    delay(5000);
+    for (int i = 0; i < STEPS_IN_FULL_REVOLUTION * stepper.getStepMultiplier(); i++) {
+        Serial.print(mySensor->measure());
+        Serial.print(" ");
+        delay(70);
+        stepper.step();
+    }
+    Serial.println("Stopped measuring");
 }
 
 void loop() {
-    stepper.changeStep(StepMotorController::SteppingMode::FULL);
-    stepper.stepDegrees(90);
-    delay(1000);
-    stepper.changeStep(StepMotorController::SteppingMode::HALF);
-    stepper.stepDegrees(90);
-    delay(1000);
-    stepper.changeStep(StepMotorController::SteppingMode::QUARTER);
-    stepper.stepDegrees(90);
-    delay(1000);
-    stepper.changeStep(StepMotorController::SteppingMode::EIGHT);
-    stepper.stepDegrees(90);
-    delay(1000);
-    stepper.changeStep(StepMotorController::SteppingMode::SIXTEENTH);
-    stepper.stepDegrees(90);
-    delay(1000);
-
-    // delay(1000);
-    // delay(50);
-    // Serial.println(mySensor->measure());
-    // stepper.step(2048);
-    // delay(1000); // Wait before repeating
-    // stepper.step(-1000);
-
-    // delay(1000);
 
 }
